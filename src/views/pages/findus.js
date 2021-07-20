14a4e013199f7e3531f5188e27a1f4eb434f144f
MiriamSaf <s3739725@student.rmit.edu.au>
2021-07-20 16:38:01 -0400

    find us reverted changes to original

--- src/views/pages/findus.js
+++ src/views/pages/findus.js
@@ -14,26 +14,6 @@ class FindusView {
 
   render(){
     const template = html`
-    <style>
-      /* Create two equal columns that floats next to each other */
-      .column {
-        float: left;
-        width: 50%;
-        padding: 10px;
-      }
-
-      .row {
-        margin: 2em;
-      }
-
-      /* Clear floats after the columns */
-      .row:after {
-        content: "";
-        display: table;
-        clear: both;
-      }
-
-    </style>
      <td-app-header user=${JSON.stringify(Auth.currentUser)}></td-app-header>
       <div> <!-- remove page content class as did not show the photo beneath it-->
       
@@ -41,120 +21,107 @@ class FindusView {
         <div id = "findus-hero-section">
         <h1 class = "findus-hero-h1">Find us</h1>
         </div><!-- close home hero -->
-
         <!-- CONTACT SECTION  -->
        
-        <div class="row">
-          <div class="column" style="background-color:#aaa;">
-            <div> <!-- div 1 emergency -->
-              <div class = "contact-col-item">
-                <h1>Emergency Contact</h1>
-                  <div class = "findus-crcl" id = "find-us-emg-span">
-                    <span>1300 886 332</span>
-                  </div>
-                  <p>If you have an emergency during your tour, please use this emergency number. For all booking enquiries, please use the relevant number for your country on this page.
-                  </p>
-              </div>
+        <div id = "section-contact-holder">
+          <div class = "contact-columns" id = "section-1-contact">
+            <div>
+            <div class = "contact-col-item">
+              <h1>Emergency Contact</h1>
+                <div class = "findus-crcl" id = "find-us-emg-span">
+                  <span>1300 886 332</span>
+                </div>
+                <p>If you have an emergency during your tour, please use this emergency number. For all booking enquiries, please use the relevant number for your country on this page.
+                </p>
             </div>
-
-            <div> <!-- div 2 - Aus -->
-              <div class = "contact-col-item">
-                <h1>Australia</h1>
-                  <div class = "findus-crcl" id ="aus-num">
-                    <span>1300 886 332</span>
-                  </div>
-                  <div class = "findus-crcl" id = "aus-num-2">
-                    <span>+61 7 30634071</span>
-                  </div>
-                  <p>Monday to Friday: 5am-6pm AEST</p>
-              </div>
-            </div> <!-- close div num 2 - UK-->
-
-             <div> <!-- div 2 - UK -->
-              <div class = "contact-col-item" id = "add-marg-col">
-                  <h1>United Kingdom</h1>
-                  <div class = "findus-crcl" id ="uk-num">
-                    <span>0845 257 5212</span>
-                  </div>
-                  <div class = "findus-crcl" id = "uk-num-2">
-                    <span>+44 (0)20 8987 3305</span>
-                  </div>
-                  <p>Monday to Friday: 12pm - 8pm. GMT</p>
-              </div><!-- close contact col item div -->
-            </div> <!-- close div num 2 - UK-->
-
-            <!-- div 3 -->
-            <div id = "add-marg-col" class = "smaller-contact">
-                  <h1>New Zealand</h1>
-                  <div class = "findus-crcl" id ="uk-num">
-                    <span>0800 525 366</span>
-                  </div>
-            </div><!-- close contact col item div -->
-
-            <!-- div 4 -->
-            <div id = "add-marg-col" class = "smaller-contact">
-                  <h1>North America</h1>
-                  <div class = "findus-crcl" id ="uk-num">
-                    <span>1 800 607 1399</span>
-                  </div>
-            </div><!-- close contact col item div -->
-
-            <!-- div 5 -->
-            <div id = "add-marg-col" class = "smaller-contact">
-                  <h1>South Africa</h1>
-                  <div class = "findus-crcl" id ="uk-num">
-                    <span>0800 982 308</span>
-                  </div>
-            </div><!-- close contact col item div -->
-            
-          </div> <!-- close col 1 -->
-
-
-          <div class="column" style="background-color:#bbb;">
+            </div>
+            <!-- div 2 -->
+            <div>
             <div id = "social-links-contact">
-              <h1>Socials</h1>
-              <div class ="social-link-div">
-                    <sl-icon name="facebook"></sl-icon>
-                    <span>TopDeckTravel</span>
-              </div><!-- close social link div -->
-
-              <div class ="social-link-div">
-                    <sl-icon name="instagram"></sl-icon>
-                    <span>topdecktravel</span>
-              </div><!-- close social link div -->
-
-              <div class ="social-link-div">
-                    <sl-icon name="twitter"></sl-icon>
-                    <span>TopdeckTravel</span>
-              </div><!-- close social link div -->
-
-              <div class ="social-link-div">
-                    <sl-icon name="youtube"></sl-icon>
-                    <span>TopDeckTravel</span>
-              </div><!-- close social link div -->
-
-              <div class ="social-link-div">
-                    <img src = "images/navy-snapchat.png" alt = "snapchat icon" class = "social-icon-img">
-                    <span>#topdecktravel</span>
-              </div><!-- close social link div -->
-
-              <div class ="social-link-div" id = "tiktok-social-div">
-                    <img src = "images/navy-tiktok.png" alt = "tiktok icon" class = "social-icon-img">
-                    <span>#topdecktravel</span>
-              </div><!-- close social link div -->
-            </div><!-- close social contact -->
-
-         
-          </div> <!-- close col 2 -->
-
-
-        </div><!-- close row-->
-
+            <h1>Socials</h1>
+            <div class ="social-link-div">
+                  <sl-icon name="facebook"></sl-icon>
+                  <span>TopDeckTravel</span>
+            </div><!-- close social link div -->
+            <div class ="social-link-div">
+                  <sl-icon name="instagram"></sl-icon>
+                  <span>topdecktravel</span>
+            </div><!-- close social link div -->
+            <div class ="social-link-div">
+                  <sl-icon name="twitter"></sl-icon>
+                  <span>TopdeckTravel</span>
+            </div><!-- close social link div -->
+            <div class ="social-link-div">
+                  <sl-icon name="youtube"></sl-icon>
+                  <span>TopDeckTravel</span>
+            </div><!-- close social link div -->
+            <div class ="social-link-div">
+                  <img src = "images/navy-snapchat.png" alt = "snapchat icon" class = "social-icon-img">
+                  <span>#topdecktravel</span>
+            </div><!-- close social link div -->
+            <div class ="social-link-div" id = "tiktok-social-div">
+                  <img src = "images/navy-tiktok.png" alt = "tiktok icon" class = "social-icon-img">
+                  <span>#topdecktravel</span>
+            </div><!-- close social link div -->
+          </div><!-- close contact item dev -->
+            </div>
+          </div>
+        
+        <!-- COLUMN 2 -->
+        <div class = "contact-columns" id = "section-2-contact">
+          <!-- first div-->  
+          <div>
+            <div class = "contact-col-item">
+            <h1>Australia</h1>
+                <div class = "findus-crcl" id ="aus-num">
+                  <span>1300 886 332</span>
+                </div>
+                <div class = "findus-crcl" id = "aus-num-2">
+                  <span>+61 7 30634071</span>
+                </div>
+                <p>Monday to Friday: 5am-6pm AEST</p>
+            </div>
+          </div><!-- close first div -->
+          
+          
+          <div> <!-- div 2 - UK -->
+            <div class = "contact-col-item" id = "add-marg-col">
+                <h1>United Kingdom</h1>
+                <div class = "findus-crcl" id ="uk-num">
+                  <span>0845 257 5212</span>
+                </div>
+                <div class = "findus-crcl" id = "uk-num-2">
+                  <span>+44 (0)20 8987 3305</span>
+                </div>
+                <p>Monday to Friday: 12pm - 8pm. GMT</p>
+            </div><!-- close contact col item div -->
+          </div> <!-- close div num 2 - UK-->
+          <!-- div 3 -->
+          <div id = "add-marg-col" class = "smaller-contact">
+                <h1>New Zealand</h1>
+                <div class = "findus-crcl" id ="uk-num">
+                  <span>0800 525 366</span>
+                </div>
+          </div><!-- close contact col item div -->
+           <!-- div 4 -->
+           <div id = "add-marg-col" class = "smaller-contact">
+                <h1>North America</h1>
+                <div class = "findus-crcl" id ="uk-num">
+                  <span>1 800 607 1399</span>
+                </div>
+          </div><!-- close contact col item div -->
+          <!-- div 5 -->
+          <div id = "add-marg-col" class = "smaller-contact">
+                <h1>South Africa</h1>
+                <div class = "findus-crcl" id ="uk-num">
+                  <span>0800 982 308</span>
+                </div>
+          </div><!-- close contact col item div -->
+        </div><!-- close section 2 col -->
+      </div>
+        
         <td-site-footer></td-site-footer>
-
-        
-        
-      </div> <!-- close page -->
+      </div>
       
     `
 
