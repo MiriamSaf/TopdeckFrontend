5631db626dbd8481088301d79eb06d8f94d35f53
MiriamSaf <s3739725@student.rmit.edu.au>
2021-07-20 20:34:18 -0400

    add until 1975 timeline page

--- src/views/pages/ourStory.js
+++ src/views/pages/ourStory.js
@@ -24,7 +24,7 @@ class OurStoryView {
         <!-- section 2 timeline -->
         <div class = "timeline-section">
           <div class = "timeline-cols">
-            <!-- timeline one -->
+            <!-- timeline one - start/ foundation -->
             <div class = "timeline-item">
               <div class = "content-timeline">
                 <h1>1973</h1>
@@ -33,7 +33,7 @@ class OurStoryView {
               </div>
             </div>
 
-            <!-- timeline two-->
+            <!-- timeline two - Bus img -->
             <div class = "timeline-item">
               <div class="line"></div> 
               <span class="dot"></span>  
@@ -44,10 +44,74 @@ class OurStoryView {
             </div>
 
             <!-- timeline three -->
-            <div class = "timeline-item">one</div>
             <div class = "timeline-item">
+              <!-- actual content-->
+              <div class = "content-timeline" class = "img-timeline">
+                  <img src = "images/OS-FESTIVAL.png" alt = "festival img">
+                </div>
+            </div>
+            
+            <!-- timeline four -1974 -->
+            <div class = "timeline-item">
+            <div class="line"></div> 
+            <span class="dot"></span> 
+              <div class = "content-timeline">
+                <h1>1974</h1>
+                <p>After the success of the first trip, Skroo and friends hit the road traversing European festivals.</p>
+              </div>
+            </div>
+
+             <!-- timeline FIVE - 1975 -->
+             <div class = "timeline-item">
+             <div class = "content-timeline">
+                <h1>1975</h1>
+                <p>Skroo and the gang continue the fun adding a trip from London to Sydney via Kathmandu.</p>
+              </div>
+             </div>
+            
+            <!-- timeline six -->
+            <div class = "timeline-item">
+            <div class="line"></div> 
             <span class="dot"></span>  
-            two</div>
+            </div>
+
+            <!-- timeline seven -->
+            <div class = "timeline-item">one</div>
+            
+            <!-- timeline eight -->
+            <div class = "timeline-item">
+            <div class="line"></div> 
+            <span class="dot"></span>  
+            </div>
+
+             <!-- timeline nine -->
+             <div class = "timeline-item">one</div>
+            
+            <!-- timeline ten -->
+            <div class = "timeline-item">
+            <div class="line"></div> 
+            <span class="dot"></span>  
+            </div>
+
+             <!-- timeline eleven -->
+             <div class = "timeline-item">one</div>
+            
+            <!-- timeline twelve -->
+            <div class = "timeline-item">
+            <div class="line"></div> 
+            <span class="dot"></span>  
+            </div>
+
+             <!-- timeline thirteen -->
+             <div class = "timeline-item">one</div>
+            
+            <!-- timeline fourteen -->
+            <div class = "timeline-item">
+            <div class="line"></div> 
+            <span class="dot"></span>  
+            </div>
+
+
           </div><!-- close timeline cols div-->
           </div>
     
