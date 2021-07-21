bf93321fa342dd1fd00e87e5c604a3d10cac2995
MiriamSaf <s3739725@student.rmit.edu.au>
2021-07-20 21:36:46 -0400

    add committed section to ourstory

--- src/views/pages/ourStory.js
+++ src/views/pages/ourStory.js
@@ -164,7 +164,11 @@ class OurStoryView {
             <span class="tl-bottom-square">
               <h1>And now in 2021</h1>
             </span>  
-           
+                <div class = "timeline-extra-content">
+                  <p>
+                  Top Deck remains committed to providing exciting travel experiences and with the COVID pandemic still affecting the world, your safety and wellbeing is our highest priority. With new flexible booking options and COVID safe itineraries, we are ready to come through the darkness and show you the world once again.
+                  </p>
+                </div>
             </div>
 
 
